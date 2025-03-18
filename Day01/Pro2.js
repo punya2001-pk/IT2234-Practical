0ff521439385855f401cea0895904ebeeae93961
Punya Udayasiri <dka.punya@gmail.com>
2025-03-18 16:25:29 +0530

    Add files via upload

---
+++ Day01/Pro2.js
@@ -0,0 +1,11 @@
+let a = 20;
+let b = 10;
+let sum = a + b;
+let sub = a - b;
+let mul = a * b;
+let div = a / b;
+
+console.log("Sum: " + sum);
+console.log("Sub: " + sub);
+console.log("Mul: " + mul);
+console.log("Div: " + div);
